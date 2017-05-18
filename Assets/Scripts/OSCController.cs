6103860cab237b47c444e4de5f30febd68c13d12
Mike Hodnick <mike@kindohm.com>
2017-05-18 17:58:16 -0500

    so I dont forget things

---
+++ Assets/Scripts/OSCController.cs
@@ -0,0 +1,99 @@
+﻿//using UnityEngine;
+//#if UNITY_EDITOR
+//using UnityEditor;
+//#endif
+//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Text;
+//using UnityOSC;
+
+//public class OSCController : MonoBehaviour
+//{
+
+//    static string TargetAddr = "127.0.0.1";
+//    static int OutGoingPort = 5001;
+//    static int InComingPort = 5000;
+//    private GameObject apple;
+//    private GameObject banana;
+//    private GameObject bottle;
+//    private Queue queue;
+
+//    void Start()
+//    {
+//        Console.WriteLine("YESSSS");
+//        OSCHandler.Instance.Init();// TargetAddr, OutGoingPort, InComingPort);
+
+//        apple = Resources.Load("Items/Apple/Apple", typeof(GameObject)) as GameObject;
+//        banana = Resources.Load("Items/Banana/Banana", typeof(GameObject)) as GameObject;
+//        bottle = Resources.Load("Items/PetBottles/Pet", typeof(GameObject)) as GameObject;
+
+//        queue = new Queue();
+//        queue = Queue.Synchronized(queue);
+
+//        var server = OSCHandler.Instance.Servers["TidalClient"].server;
+//        server.PacketReceivedEvent += OnPacketReceived;
+//    }
+
+//    void OnPacketReceived(OSCServer server, OSCPacket packet)
+//    {
+//        queue.Enqueue(packet);
+//    }
+
+//    void Update()
+//    {
+//        if (queue == null)
+//        {
+//            return;
+//        }
+//        while (0 < queue.Count)
+//        {
+//            OSCPacket packet = queue.Dequeue() as OSCPacket;
+//            if (packet.IsBundle())
+//            {
+//                // if OSCBundl
+//                OSCBundle bundle = packet as OSCBundle;
+//                foreach (OSCMessage msg in bundle.Data)
+//                {
+
+//                }
+//            }
+//            else
+//            {
+//                OSCMessage msg = packet as OSCMessage;
+
+//                if ("/test" == msg.Address)
+//                {
+//                    //print (msg.Data[1].ToString());
+//                    var thing = msg.Data[1].ToString();
+//                    var pos = new Vector3((float)msg.Data[2], (float)msg.Data[3], (float)msg.Data[4]);
+//                    var dur = (float)packet.Data[5];
+//                    AppendItem(thing, pos, dur);
+//                }
+//            }
+//        }
+//    }
+
+//    void AppendItem(String item, Vector3 pos, float dur)
+//    {
+//        GameObject thing = apple;
+//        switch (item)
+//        {
+//            case "apple":
+//                thing = apple;
+//                break;
+//            case "banana":
+//                thing = banana;
+//                break;
+//            case "bottle":
+//                thing = bottle;
+//                break;
+//        }
+//        if (thing == null)
+//        {
+//            return;
+//        }
+//        var clone = Instantiate(thing, pos, Quaternion.identity);
+//        Destroy(clone, dur);
+//    }
+//}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityOSC;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    private Queue queue;
+    GameObject cube;
+    GameObject cylinder;
+    GameObject sphere;
+    GameObject capsule;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+        Debug.Log("teh player");
+
+        OSCHandler.Instance.Init();// TargetAddr, OutGoingPort, InComingPort);
+
+        queue = new Queue();
+        queue = Queue.Synchronized(queue);
+
+        var server = OSCHandler.Instance.Servers["TidalClient"].server;
+        server.PacketReceivedEvent += OnPacketReceived;
+
+        Debug.Log("Loading shapes.");
+        cube = Resources.Load("Cube", typeof(GameObject)) as GameObject;
+        cylinder = Resources.Load("Cylinder", typeof(GameObject)) as GameObject;
+        capsule = Resources.Load("Capsule", typeof(GameObject)) as GameObject;
+        sphere = Resources.Load("Sphere", typeof(GameObject)) as GameObject;
+
+    }
+
+    void OnPacketReceived(OSCServer server, OSCPacket packet)
+    {
+        Debug.Log("recieved");
+        queue.Enqueue(packet);
+    }
+
+    void Update()
+    {
+        //Debug.Log("Updating");
+        if (queue == null)
+        {
+            //Debug.Log("nada");
+            return;
+        }
+
+
+        while (0 < queue.Count)
+        {
+            
+            //Debug.Log("HAS STUFF");
+            OSCPacket packet = queue.Dequeue() as OSCPacket;
+            if (packet.IsBundle())
+            {
+                // if OSCBundl
+                OSCBundle bundle = packet as OSCBundle;
+                foreach (OSCMessage msg in bundle.Data)
+                {
+
+                }
+            }
+            else
+            {
+                OSCMessage msg = packet as OSCMessage;
+                //Debug.Log(msg.Address);
+                if ("/unity_osc" == msg.Address)
+                {
+                    //print (msg.Data[1].ToString());
+                    var thing = msg.Data[1].ToString();
+                    var pos = new Vector3((float)msg.Data[2], (float)msg.Data[3], (float)msg.Data[4]);
+
+                    //rb.MovePosition(pos);
+
+                    var dur = (float)packet.Data[5];
+
+                    AppendItem(thing, pos, dur);
+                }
+            }
+        }
+
+
+    }
+
+    void AppendItem(String item, Vector3 pos, float dur)
+    {
+        Debug.Log("AppendItem()");
+        GameObject thing = null;
+        switch (item)
+        {
+            case "cube":
+                thing = cube;
+                break;
+            case "sphere":
+                thing = sphere;
+                break;
+            case "capsule":
+                thing = capsule;
+                break;
+            case "cylinder":
+                thing = cylinder;
+                break;
+        }
+
+        if (thing == null)
+        {
+            return;
+        }
+        
+        var rotation = new Quaternion(UnityEngine.Random.Range(-10, 10), UnityEngine.Random.Range(-10, 10), UnityEngine.Random.Range(-10, 10), UnityEngine.Random.Range(-10, 10));
+        
+        var clone = Instantiate(thing, pos, rotation);
+        clone.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+
+        Destroy(clone, dur);
+    }
+
+}
